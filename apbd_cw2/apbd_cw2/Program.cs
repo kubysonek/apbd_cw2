4011ac736dc41a5e0b440638ddb3d27db080f42f
Jakub Tanski <jakub.tanski@yoc.com>
2025-03-16 15:57:40 +0100

    initial project setup with few variables

---
+++ apbd_cw2/apbd_cw2/Program.cs
@@ -0,0 +1,12 @@
+﻿{
+    int number = 10;
+    string text = "Text: " + number + ".";
+    string textWithDollar = $"TEst: {number}.";
+
+    var k = "test";
+    
+    //Nullable
+    int? nullableInt = null;
+    nullableInt = 10;
+    Object? o = null;
+}
